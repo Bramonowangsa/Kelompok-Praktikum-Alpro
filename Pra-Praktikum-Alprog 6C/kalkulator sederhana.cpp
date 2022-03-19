506e678ea4a0c3d19a6af41b480f18866066d260
Bramonowangsa <bramonowangsa2002@gmail.com>
2022-03-19 10:17:45 +0700

    pra praktikum alpro

---
+++ Pra-Praktikum-Alprog 6C/kalkulator sederhana.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <conio.h>
+#include <string>
+
+using namespace std;
+
+int main(){
+	
+	int bil1,bil2, pil;
+	float hasil;
+	string operasi;
+	
+	cout<<"PILIH OPERATOR : "<<endl;
+	cout<<"1. Penjumlahan"<<endl;
+	cout<<"2. Pengurangan"<<endl;
+	cout<<"3. Perkalian"<<endl;
+	cout<<"4. Pembagian"<<endl;
+	cout<<endl;
+	
+	cout<<"Masukan Pilihan : ";
+	cin>>pil;
+	cout<<"Masukan Bilangan pertama : ";
+	cin>>bil1;
+	cout<<"Masukan Bilangan kedua   : ";
+	cin>>bil2;	
+	
+	switch(pil){
+	    case 1 : 
+			hasil=bil1+bil2;
+			operasi='+';
+			break;
+		case 2 : 
+			hasil=bil1-bil2;
+			operasi='-';
+			break;
+		case 3 : 
+			hasil=bil1*bil2;
+			operasi='*';
+			break;
+		case 4 : 
+			hasil=bil1/bil2;
+			operasi='/';
+			break;
+		default :
+			cout<<"Salah Memasukan Operator"<<endl;
+	}
+	cout<<endl;
+	cout<<"Hasil : "<<endl;
+	cout<<"-----------------------------"<<endl;
+	cout<<"   "<<bil1<<operasi<<bil2<<"="<<hasil<<endl;
+	cout<<"-----------------------------"<<endl;
+	
+	getch();
+}
